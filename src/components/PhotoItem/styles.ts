bbe2c727a7cc3e167ba7ef1a28355091700bc6eb
William Viana <willviana.batera@gmail.com>
2021-12-03 17:13:28 -0300

    first commit for repository

---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #3d3f43;
+    border-radius: 10px;
+    padding: 10px;
+
+    img {
+        max-width: 100%;
+        height: auto;
+        display: block;
+        margin-bottom: 10px;
+        border-radius: 10px;
+    }
+
+    button {
+        display: block;
+        background-color:#756DF4;
+        border: 0;
+        color: #fff;
+        padding: 8px 16px;
+        font-size: 15px;
+        border-radius: 10px;
+        margin: 10px auto 0 auto;
+        cursor: pointer;
+        
+        &:hover {
+            opacity: .9;
+        }
+    }
+`;
